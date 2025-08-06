cfcdfdc33cc0c896209a73e132cb6234819a4f37
askip <azkaghalib2810@gmail.com>
2025-08-06 12:49:04 +0700

    kelengkapan data asesor

--- app/src/main/java/com/example/mylsp/screen/asesor/KelengkapanDataAsesor.kt
+++ app/src/main/java/com/example/mylsp/screen/asesor/KelengkapanDataAsesor.kt
@@ -1,8 +1,10 @@
 package com.example.mylsp.screen.asesor
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,7 +12,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -69,17 +74,150 @@ fun KelengkapanDataAsesor(modifier: Modifier = Modifier, navController: NavContr
                 onValueChange = {
                     namaLengkap = it
                 },
-                placeholder = { "Nama Lengkap" },
+                placeholder = { Text("Nama Lengkap") },
                 modifier = Modifier.fillMaxWidth(),
                 shape = RoundedCornerShape(16.dp)
             )
 
+            Text(
+                "Tanggal Lahir",
+                Modifier.fillMaxWidth()
+            )
+
+            OutlinedTextField(
+                value = tanggalLahir,
+                onValueChange = {
+                    tanggalLahir = it
+                },
+                placeholder = { Text("Tanggal Lahir") },
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(16.dp)
+            )
+
+            Text(
+                "Alamat",
+                Modifier.fillMaxWidth()
+            )
+
+            OutlinedTextField(
+                value = alamat,
+                onValueChange = {
+                    alamat = it
+                },
+                placeholder = { "Nama Lengkap" },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp),
+                shape = RoundedCornerShape(16.dp)
+            )
+
+            Text(
+                "Jenis Kelamin",
+                Modifier.fillMaxWidth()
+            )
+
+            Row(modifier = Modifier.fillMaxWidth()) {
+                RadioButton(
+                    selected = jenkel == "pria",
+                    onClick = { jenkel = "pria" }
+                )
+                Text("Pria")
+
+                RadioButton(
+                    selected = jenkel == "wanita",
+                    onClick = { jenkel = "wanita" }
+                )
+                Text("Wanita")
+            }
+
+            Text(
+                "Pekerjaan",
+                Modifier.fillMaxWidth()
+            )
+
+            OutlinedTextField(
+                value = pekerjaan,
+                onValueChange = {
+                    pekerjaan = it
+                },
+                placeholder = { Text("Pekerjaan") },
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(16.dp)
+            )
+
+            Text(
+                "KTP",
+                Modifier.fillMaxWidth()
+            )
+
+            OutlinedTextField(
+                value = ktp,
+                onValueChange = {
+                    ktp = it
+                },
+                placeholder = { Text("Masukkan foto KTP anda") },
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(16.dp)
+            )
+
+            Text(
+                "Ijazah Terakhir",
+                Modifier.fillMaxWidth()
+            )
+
+            OutlinedTextField(
+                value = ijazahTerakhir,
+                onValueChange = {
+                    ijazahTerakhir = it
+                },
+                placeholder = { Text("Masukkan foto ijazah terakhir anda") },
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(16.dp)
+            )
+            Text(
+                "Sertifikat Kompetensi Keahlian",
+                Modifier.fillMaxWidth()
+            )
+
+            OutlinedTextField(
+                value = sertifikatKompetensi,
+                onValueChange = {
+                    sertifikatKompetensi = it
+                },
+                placeholder = { Text("Masukkan foto sertifikat kompetensi keahlian anda") },
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(16.dp)
+            )
+
+
+            Text(
+                "Foto 3x4",
+                Modifier.fillMaxWidth()
+            )
+
+            OutlinedTextField(
+                value = foto,
+                onValueChange = {
+                    foto = it
+                },
+                placeholder = { Text("Masukkan foto 3x4 anda") },
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(16.dp)
+            )
+
+            Spacer(Modifier.height(50.dp))
+
             Button(
                 onClick = {
-                    navController.navigate("tanda_tangan_asesor")
-                }
+                    navController.navigate("event")
+                },
+                shape = RoundedCornerShape(5.dp),
+                modifier = Modifier
+                    .background(color = MaterialTheme.colorScheme.onBackground)
             ) {
-                Text("Kirim Data")
+                Row {
+                    Text("Simpan Data")
+                }
             }
         }
     }
